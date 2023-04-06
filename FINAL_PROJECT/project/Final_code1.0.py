6fc446bf21fabf348674d810c4b928fec487de6e
YoussefElShahid <124705016+YoussefElShahid@users.noreply.github.com>
2023-04-05 22:18:44 -0400

    Update Final_code1.0.py

--- FINAL_PROJECT/project/Final_code1.0.py
+++ FINAL_PROJECT/project/Final_code1.0.py
@@ -15,14 +15,15 @@ COLOR_SENSOR=EV3ColorSensor(2)
 TOUCH_SENSOR=TouchSensor(4)
 LEFT_MOTOR.set_limits(dps = 500)
 RIGHT_MOTOR.set_limits(dps = 500)
+#PUSHING_MOTOR.set_limits(dps=150)
 
-TARGET=(170+25)/2
-Kp=0.01
-Ki=0.0015
-Kd=0.02
+TARGET=(21+184)/2
+Kp=0.1
+Ki=0.08
+Kd=0
 def real_value(sensor):
     value = sensor.get_value()
-    while sensor.get_value()== None or sensor.get_value() ==[0,0,0,0]:
+    while value== None or value ==[0,0,0,0]:
         value=sensor.get_value()
         print(value)
     print(value)
@@ -38,14 +39,18 @@ def PID(value,sign,base_speed):
     integral+=error
     derivative=error-last_error
     turn_rate=Kp*error+Ki*integral+Kd*derivative
-
-    left_motor.set_power((base_speed+(sign*turn_rate))*-1)
-    right_motor.set_power((base_speed-(sign*turn_rate))*-1) 
+    
+    if color_choosing(value[:-1])==3:
+        LEFT_MOTOR.set_power(0)
+        RIGHT_MOTOR.set_power(-18)
+    else:
+        LEFT_MOTOR.set_power((base_speed+(sign*turn_rate))*-1)
+        RIGHT_MOTOR.set_power((base_speed-(sign*turn_rate))*-1)
     
 def delivery_protocol(degrees):
     DELIVERY_MOTOR.set_position(degrees)
     sleep(2)
-    PUSHING_MOTOR.set_position(165)
+    PUSHING_MOTOR.set_position(155)
     sleep(0.5)
     PUSHING_MOTOR.set_position(0)
     sleep(0.5)
@@ -64,50 +69,47 @@ def delivery(color):
         delivery_protocol(-570)
     elif color == 6:
         delivery_protocol(-300)
-def forwards(left_motor,right_motor,greens):
+def forwards(greens):
     base_speed=25
     value=real_value(LINE_SENSOR)
+    print(value)
     if color_choosing(value[:-1])==4:
         greens+=1
         print('green detected')
         while color_choosing(value[:-1])==4:
-            left_motor.set_power(-10)
-            right_motor.set_power(-10)
+            LEFT_MOTOR.set_power(-10)
+            RIGHT_MOTOR.set_power(-10)
             value=real_value(LINE_SENSOR)
             sleep(0.1)
-    if color_choosing(value[:-1])==3:
-        left_motor.set_power((base_speed)*-1)
-        right_motor.set_power(0)
     PID(value,1,base_speed)
     return greens
-def backwards(left_motor,right_motor,input_list):
-    base_speed=15
-    
+def backwards(input_list):
+    base_speed=13
+    trigger=0
     value=real_value(LINE_SENSOR)
     col=real_value(COLOR_SENSOR)
     if color_choosing(value[:-1])==4:
-        left_motor.set_power(-10)
-        right_motor.set_power(-10)
-        while color_choosing(col[:-1])==7:
-            left_motor.set_power(-10)
-            right_motor.set_power(-10)
+        trigger+=1
+        while color_choosing(value[:-1])==4:
+            LEFT_MOTOR.set_power(-base_speed)
+            RIGHT_MOTOR.set_power(-base_speed)
+            value=real_value(LINE_SENSOR)
+    if trigger == 1:
+        col=real_value(COLOR_SENSOR)
+        while color_choosing(col)==7:
             col=real_value(COLOR_SENSOR)
-            sleep(0.1)
-            print(col)
         print(color_choosing(col[:-1]))
         if color_choosing(col[:-1]) not in input_list:
             input_list.append(color_choosing(col[:-1]))
-            left_motor.set_power(0)
-            right_motor.set_power(0)
+            LEFT_MOTOR.set_power(0)
+            RIGHT_MOTOR.set_power(0)
             sleep(0.5)
-            left_motor.set_position_relative(-270)
-            right_motor.set_position_relative(-270)
+            #LEFT_MOTOR.set_position_relative(-270)
+            #RIGHT_MOTOR.set_position_relative(-270)
             sleep(0.5)
             delivery(color_choosing(col[:-1]))
             sleep(0.5)
-            left_motor.set_position_relative(240)
-            right_motor.set_position_relative(240)
-            sleep(1)
+            trigger-=1
     value=real_value(LINE_SENSOR)
     PID(value,-1,base_speed)
     print(input_list) 
@@ -117,25 +119,29 @@ if __name__ == "__main__":
         greens_detected=0
         deliveries_done=[]
         while True:
-            if greens_detected<6:
-                greens_detected=forwards(LEFT_MOTOR,RIGHT_MOTOR,greens_detected)
+            '''if greens_detected<6:
+                greens_detected=forwards(greens_detected)
             elif greens_dected == 6:
-                left_motor.set_position_relative(-360)
-                right_motor.set_position_relative(-360)
+                LEFT_MOTOR.set_position_relative(-360)
+                RIGHT_MOTOR.set_position_relative(-360)
                 sleep(0.5)
-                left_motor.set_position_relative(-500)
+                LEFT_MOTOR.set_position_relative(-500)
                 sleep(0.5)
-                backwards(LEFT_MOTOR,RIGHT_MOTOR,deliveries_done)
+                backwards(deliveries_done)
                 greens_detected+=1
             else:
-                backwards(LEFT_MOTOR,RIGHT_MOTOR,deliveries_done)
+                backwards(deliveries_done)
             if len(deliveries_done)==6:
-                break
+                break'''
+            backwards(deliveries_done)
+            #print(LINE_SENSOR.get_value())
+            #value=real_value(LINE_SENSOR)
     except BaseException as e :
-        print ("Done with the program")
+        print("Done with the program")
         print(e)
         print(traceback.format_exc())
         LEFT_MOTOR.set_power(0)
         RIGHT_MOTOR.set_power(0)
         reset_brick()
         exit()
+
